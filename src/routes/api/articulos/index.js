51cc00a299adacb55daf2c5fabb1f03c18b75d08
Mario Ruano <mrf1989@hotmail.com>
2022-06-06 20:09:24 +0200

    Configura la URI de la API en la session

--- src/routes/api/articulos/index.js
+++
@@ -1,9 +0,0 @@
-export const get = async ({ request }) => {
-    const headers = new Headers(request.headers);
-    headers.set("referer", `${request.headers.referer}/api/articulos`);
-    const response = await fetch(`${process.env.VITE_MUEVETE_API_URI}/api/articulos`, {
-        headers
-    });
-        
-    return response;
-}
