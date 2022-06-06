6871c76bf4676cbc2a525d26dae60e9bfe6b6a41
Mario Ruano <mrf1989@hotmail.com>
2022-06-06 13:16:10 +0200

    Layout principal base

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script>
+import Navbar from "$lib/Navbar.svelte";
+import Footer from "$lib/Footer.svelte";
+</script>
+
+<div class="container">
+    <Navbar />
+    <div class="row">
+        <slot></slot>
+    </div>
+    <Footer />
+</div>
