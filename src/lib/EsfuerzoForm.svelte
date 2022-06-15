7df96e35313d6eee73c7e711d5446b791f915153
Mario Ruano <mrf1989@hotmail.com>
2022-06-15 21:53:55 +0200

    Añade esfuerzos a los eventos mediante dorsal
    Hay que implementar la regla de negocio para que limite añadir kilómetros a eventos que están completos.
    
    Para ello se tendrá que implemtar una restricción en la API, que enviará una excepeción y que será tratada en el frontend.

---
+++ src/lib/EsfuerzoForm.svelte
@@ -0,0 +1,55 @@
+<script>
+import { createEventDispatcher } from "svelte";
+
+export let dorsal;
+
+let numKm, comentario, modalidad, dorsal_id = dorsal._id;
+
+const dispatch = createEventDispatcher();
+
+const getDatosEsfuerzo = () => {
+    dispatch("post", { numKm, comentario, modalidad, dorsal_id });
+}
+</script>
+
+<div class="modal fade" id="esfuerzoForm" tabindex="-1" role="dialog">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Añadir esfuerzo realizado</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-group">
+                        <label for="comentario">Comentario</label>
+                        <textarea bind:value={comentario} class="form-control" rows="3" id="lema"></textarea>
+                    </div>
+                    <div class="form-row">
+                        <div class="col-md-6">
+                            <label for="numKm">Kilómetros</label>
+                            <input bind:value={numKm} type="number" name="numKm" id="numKm" class="form-control">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="modalidad">Modalidad</label>
+                            <select bind:value={modalidad} class="form-control" name="modalidad" id="modalidad">
+                                <option disabled>Selecciona</option>
+                                <option value="caminata">Caminata</option>
+                                <option value="carrera">Carrera</option>
+                                <option value="bicicleta">Bicicleta</option>
+                                <option value="natacion">Natación</option>
+                            </select>
+                        </div>
+                        <input bind:value={dorsal_id} type="hidden" name="dorsal_id">
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                <button on:click={getDatosEsfuerzo} type="button" class="btn btn-primary" data-dismiss="modal">Añadir esfuerzo</button>
+            </div>
+        </div>
+    </div>
+</div>
